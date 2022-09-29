ea65de99c76e79cd441c03c0e4ec8ecf265763b3
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-09-29 18:37:55 +0900

    Initial commit

---
+++ src/main/java/com/example/ec_mall/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.ec_mall;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index(){
+        return "Greetings from Spring Boot!";
+    }
+}
