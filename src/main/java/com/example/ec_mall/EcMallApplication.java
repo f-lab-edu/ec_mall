60bda1c9ecce51822b8382d709e82789c8629cd7
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-09-29 16:36:57 +0900

    Initial commit

---
+++ src/main/java/com/example/ec_mall/EcMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcMallApplication.class, args);
+	}
+
+}
