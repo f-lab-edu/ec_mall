155381ef369fea9bfc7fc0a5a08b02fc89454429
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-10-05 21:12:12 +0900

    Initial commit

--- src/main/java/com/example/ec_mall/HelloController.java
+++ src/main/java/com/example/ec_mall/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index(){
-        return "Greetings from Spring Boot! (checked by sy)_TEST";
+        return "Greetings from Spring Boot! (checked by sy)_TEST!!!";
     }
 }
