9f57400c2832fdaa36f3dd3134d6c8fd35a2ae72
sy_patrick <booth20037@gmail.com>
2022-11-16 13:42:15 +0900

    회원가입 API 수정(MergeConflict)_221116

--- src/main/java/com/example/ec_mall/exception/ErrorCode.java
+++ src/main/java/com/example/ec_mall/exception/ErrorCode.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    //800 회원가입 - 규칙에 위배된 정보 입력
+    //700 공통 - 규칙에 위배된 정보 입력
     INVALID_INPUT_VALUE(700,""),
     //801 회원가입 - 중복된 이메일
     ALREADY_SAVED_EMAIL(801, "이미 가입된 이메일입니다.");
