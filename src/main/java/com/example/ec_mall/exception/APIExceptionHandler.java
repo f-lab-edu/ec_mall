7fcb32bab0b252624a39fa5bc918a822747b20e1
sy_patrick <booth20037@gmail.com>
2022-11-16 13:38:43 +0900

    회원가입 API 수정(MergeConflict)_221116

--- src/main/java/com/example/ec_mall/exception/APIExceptionHandler.java
+++ src/main/java/com/example/ec_mall/exception/APIExceptionHandler.java
@@ -2,6 +2,8 @@ package com.example.ec_mall.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,16 +23,22 @@ public class APIExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        BindingResult bindingResult = e.getBindingResult(); // BindingResult 은 검증오류를 보관하는 객체이다.
 
+        /*
+         *   StringBuffer, StringBuilder 클래스
+         *     => StringBuffer Thread safe 하며, StringBuilder Thread safe 하지 않다.
+		          append() 메소드를 이용해서 값을 더할 수 있으며, 문자열을 더하더라도 새로운 객체를 생성하지 않는다.
+         */
+        StringBuffer stringBuffer = new StringBuffer();
 
-        ErrorCode errorCode = ErrorCode.INVALID_INPUT;
-
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getStatus())
-                .message(e.getBindingResult().getFieldError().getDefaultMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            stringBuffer.append("[Status:" + errorCode.getStatus()).append(", ");
+            stringBuffer.append(fieldError.getField()).append(":");
+            stringBuffer.append(fieldError.getDefaultMessage()).append("]");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(stringBuffer.toString());
     }
 
 }
