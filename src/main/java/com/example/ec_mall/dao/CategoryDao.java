9f9bb5942c6dbda4a163ba590dd649e666b9a1c3
sypatrick <booth20037@gmail.com>
2022-11-28 13:05:58 +0900

    상품 수정 API_221128

--- src/main/java/com/example/ec_mall/dao/CategoryDao.java
+++ src/main/java/com/example/ec_mall/dao/CategoryDao.java
@@ -3,6 +3,7 @@ package com.example.ec_mall.dao;
 import com.example.ec_mall.dto.enums.categoryEnum;
 import lombok.Builder;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -11,7 +12,7 @@ public class CategoryDao {
     private categoryEnum bigCategory;
     private String smallCategory;
     private String createdBy;
-    private String createdDate;
+    private LocalDateTime createdDate;
     private String updatedBy;
-    private String updatedDate;
+    private LocalDateTime updatedDate;
 }
