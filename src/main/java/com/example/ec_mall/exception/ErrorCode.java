5a4acb636cf6ebb7d6376576bb90f76f9daf2804
sypatrick <booth20037@gmail.com>
2022-12-13 21:11:14 +0900

    주문 API_221213 (Test Code 미작성)

--- src/main/java/com/example/ec_mall/exception/ErrorCode.java
+++ src/main/java/com/example/ec_mall/exception/ErrorCode.java
@@ -14,9 +14,11 @@ public enum ErrorCode {
     ALREADY_SAVED_EMAIL(801, "이미 가입된 이메일입니다."),
     NOT_FOUND_ACCOUNT(802, "아이디 또는 비밀번호를 확인해주세요."),
 
-    //900번대 상품
+    //900번대 상품 및 주문
     //901 - 없는 상품
-    NOT_FOUND_PRODUCT(901, "없는 상품입니다.");
+    NOT_FOUND_PRODUCT(901, "없는 상품입니다."),
+    //910 - 재고 확인
+    NOT_ENOUGH_PRODUCT(910, "재고를 확인해주세요");
 
     private final int status;
     private final String message;
--- src/main/java/com/example/ec_mall/service/OrderService.java
+++ src/main/java/com/example/ec_mall/service/OrderService.java
@@ -3,16 +3,32 @@ package com.example.ec_mall.service;
 import com.example.ec_mall.dao.OrderDao;
 import com.example.ec_mall.dao.ProductOrders;
 import com.example.ec_mall.dto.request.OrderRequestDTO;
+import com.example.ec_mall.exception.APIException;
+import com.example.ec_mall.exception.ErrorCode;
 import com.example.ec_mall.mapper.OrderMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class OrderService {
     private final OrderMapper orderMapper;
 
     public void order(String email, OrderRequestDTO orderRequestDTO, long id){
+        /**
+         * 결제등의 추가 행위는 추후에 작업하고 현재는 product 테이블의 stock을 감소시키는 것만 한다.
+         * 주문 후 재고가 0보다 작다면 exception 발생
+         */
+        // beforeStock : 주문 전 재고
+        int beforeStock = orderMapper.findStockByProductId(id);
+        // afterStock : 주문 후 재고
+        int afterStock = beforeStock - orderRequestDTO.getOrdersCount();
+        if(afterStock < 0) {
+            log.error("주문한 수량만큼 재고가 없습니다. 주문 가능한 수량 : {}", beforeStock);
+            throw new APIException(ErrorCode.NOT_ENOUGH_PRODUCT);
+        }
 
         OrderDao orderDao = OrderDao.builder()
                 .accountId(orderMapper.findAccountByEmail(email))
@@ -30,16 +46,6 @@ public class OrderService {
                 .updatedBy(email)
                 .build();
         orderMapper.addProductOrder(productOrders);
-
-        /**
-         * 결제등의 추가 행위는 추후에 작업하고 현재는 product 테이블의 stock을 감소시키는 것만 한다.
-         */
-        // beforeStock : 주문 전 재고
-        int beforeStock = orderMapper.findStockByProductId(id);
-        // afterStock : 주문 후 재고
-        int afterStock = beforeStock - orderRequestDTO.getOrdersCount();
-        if(beforeStock - afterStock > 0) {
-            orderMapper.subtractionStock(id, afterStock);
-        }
+        orderMapper.subtractionStock(id, afterStock);
     }
 }
